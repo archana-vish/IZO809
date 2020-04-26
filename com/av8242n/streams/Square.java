a70f16eca0c8b86f37d5aa939c7f197d31a869d9
archanav <archana.vish@yahoo.in>
2020-04-26 12:53:30 +0100

    Initial Commit

--- com/av8242n/streams/Square.java
+++ com/av8242n/streams/Square.java
@@ -2,6 +2,7 @@ package com.av8242n.streams;
 
 import java.util.stream.DoubleStream;
 
+// Push to develop
 public class Square {
     public static void main(String[] args) {
        double sum = DoubleStream.of(1.0, 4.0, 9.0)
